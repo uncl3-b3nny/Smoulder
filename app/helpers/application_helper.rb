3f8fd138fd9058e719616d75a683a3fad80ce706
bjlinville1 <Jgrant@jennifers-mbp.hsd1.sc.comcast.net>
2014-10-02 10:06:02 -0400

    fixed login links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
 
     def user_login_links
     if user_signed_in?
-      link_to "Log Out", destroy_user_session_path
+      link_to "Log Out", destroy_user_session_path, method: :delete
     else
       link_to "Log In", new_user_session_path
     end
