34103569dbe9e9123029ddefc1a1ccb636f6e7dd
bjlinville1 <Jgrant@Jennifers-MacBook-Pro.local>
2014-10-08 09:09:35 -0400

    fixed the bug where the pictures disappear after ajax button is clicked

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -5,15 +5,19 @@ $(document).ready(function() {
       // put your options and callbacks here
        events: '/events.json'
   })
-  $(document).on("click", '.update_event',function() {
+  $(document).on("click", '.update_event',function(event) {
+    event.preventDefault();
     $("#menu-item-Calendar a").click()
     console.log("its working!!!")
-    console.log($(this).attr("suggestion_frame_partial"))
+    console.log($(this).attr("route_to"))
     $.ajax({
-      url: $(this).attr("suggestion_frame_partial"),
+      url: $(this).attr("route_to"),
       type: 'PATCH',
       dataType: 'script',
-      data: {event: {title: $(this).attr('event_title'), start: $(this).attr('start'), event_type: $(this).attr('event_type'), user_id: $(this).attr('user_id'), workflow_state: "done"} } 
+      data: {event: {title: $(this).attr('event_title'), start: $(this).attr('start'), event_type: $(this).attr('event_type'), user_id: $(this).attr('user_id'), workflow_state: "done"} },
+      success: function(res) {
+         load_images("lazy", true, true);
+      } 
     })
     
   });
